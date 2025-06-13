18cb9e425947b9698b1553f0fa6e11412849946d
kaganulusoy <95186940+kaganulusoy@users.noreply.github.com>
2025-06-13 12:09:18 +0300

    Update Thermocouple_Logging.py
    gpio 8 busy debug

--- Thermocouple_Logging.py
+++ Thermocouple_Logging.py
@@ -1,15 +1,20 @@
 import time
 import board
+import busio
 import digitalio
 import adafruit_max31856
 
-# SPI ve Chip Select pinini ayarla
-spi = board.SPI()  # GPIO11 (SCLK), GPIO10 (MOSI), GPIO9 (MISO)
-cs = digitalio.DigitalInOut(board.D8)  # GPIO8 (CE0)
+# SPI arayüzü başlat
+spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 
-# Sensor
+# Chip Select pini ayarla
+cs = digitalio.DigitalInOut(board.D8)  # CE0 → GPIO8
+cs.direction = digitalio.Direction.OUTPUT
+
+# Sensör nesnesi oluştur
 sensor = adafruit_max31856.MAX31856(spi, cs)
 
+# Sürekli okuma döngüsü
 while True:
     try:
         sicaklik = sensor.temperature
